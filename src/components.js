d859f2fc4c240687c29cf12e8d183d2f86da5f1a
Graham Holtslander <gholtslander@vendasta.com>
2017-01-12 09:22:15 -0600

    Myriad changes for the better of the duck

---
+++ src/components.js
@@ -0,0 +1,77 @@
+(function (Vue) {
+  'use strict';
+
+  Vue.component('name-picker', {
+    data: function () {
+      return {
+        username: 'You',
+        editingName: false
+      }
+    },
+    template: `
+    <div class="pure-u-1-1">
+      <form v-show="editingName" v-on:submit.prevent="editingName = false" class="pure-form pure-form-stacked" style="display: inline-block">
+        <label for="username">Change your username:</label>
+        <input type="text" v-model="username" id="username"/>
+        <button type="submit" class="pure-button pure-button-primary">Change</button>
+      </form>
+      <span v-show="!editingName" v-on:dblclick="editingName = true" title="Double click to change your username">Messages will come from: {{ username }}</span>
+    </div>
+    `,
+    watch: {
+      editingName: function (isEditing) {
+        if (!isEditing) {
+          // They've finished editing their name, emit the new one.
+          this.$emit('namechanged', this.username);
+        }
+      }
+    }
+  });
+
+  Vue.component('chat-box', {
+    data: function () {
+      return {
+        textValue: ''
+      }
+    },
+    template: `
+  <div class="pure-u-1-1">
+    <form class="pure-form" v-on:submit.prevent="emitNewMessage">
+      <input type="text" v-model="textValue" class="pure-u-18-24" />
+      <button type="submit" class="pure-button pure-button-primary pure-u-4-24">Send</button>
+    </form>
+  </div>
+  `,
+    methods: {
+      emitNewMessage: function () {
+        this.$emit('newmessage', this.textValue);
+        this.textValue = '';
+      }
+    }
+  });
+
+  Vue.component('message-area', {
+    props: ['messages'],
+    template: `
+  <div v-bind:style="styleObject" class="pure-u-1-1">
+    <div v-for="message in messages">
+      <div style="clear: both">
+        <span style="float:left;">{{ message.authorName }}: {{message.message}}</span>
+        <span style="float: right;">{{ message.timestamp }}</span>
+      </div>
+    </div>
+  </div>
+  `,
+    data: function () {
+      return {
+        styleObject: {
+          border: "1px solid #DADADA",
+          height: "500px",
+          padding: "5px",
+          "overflow-y": "scroll",
+          "background-color": "rgba(255,255,255,0.5)"
+        }
+      }
+    }
+  });
+})(window.Vue);
--- src/ducker.js
+++ src/ducker.js
@@ -1,84 +1,20 @@
 (function (Vue) {
   'use strict';
 
-  Vue.component('name-picker', {
-    data: function () {
-      return {
-        username: 'You',
-        editingName: false
-      }
-    },
-    template: `
-    <div class="pure-u-1-1">
-      <form class="pure-form pure-form-stacked" v-show="editingName" style="display: inline-block"  v-on:submit.prevent="editingName = false">
-        <label for="username">Change your username:</label>
-        <input type="text" v-model="username" id="username"/>
-        <button type="submit" class="pure-button pure-button-primary">Change</button>
-      </form>
-      <span v-show="!editingName" v-on:dblclick="editingName = true">Username: {{ username }} (double click to change your username)</span>
-    </div>
-    `,
-    computed: {
-      allCapsUsername: function () {
-        return this.username.toUpperCase();
-      }
-    },
-    watch: {
-      editingName: function (isEditing) {
-        if (!isEditing) {
-          // They've finished editing their name, emit the new one.
-          this.$emit('newname', this.username);
-        }
-      }
-    }
-  });
+  const duckPhrases = [
+    "Hmmm.",
+    "Okay.",
+    "I see.",
+    "Alright.",
+    "Interesting.",
+    "OK",
+    "I'm with you.",
+    "Following.",
+    "Huh.",
+    "Got it."
+  ];
 
-  Vue.component('chat-box', {
-    data: function () {
-      return {
-        textValue: ''
-      }
-    },
-    template: `
-  <div class="pure-u-1-1">
-    <form class="pure-form" v-on:submit.prevent="emitNewMessage">
-      <input type="text" v-model="textValue" class="pure-input-1" />
-      <button type="submit" class="pure-button pure-button-primary">Send</button>
-    </form>
-  </div>
-  `,
-    methods: {
-      emitNewMessage: function () {
-        this.$emit('newmessage', this.textValue);
-        this.textValue = '';
-      }
-    }
-  });
-
-  Vue.component('message-area', {
-    props: ['messages'],
-    template: `
-  <div v-bind:style="styleObject" class="pure-u-1-1">
-    <div v-for="message in messages">
-      <div style="clear: both">
-        <span style="float:left;">{{ message.authorName }}: {{message.message}}</span>
-        <span style="float: right;">{{ message.timestamp }}</span>
-      </div>
-    </div>
-  </div>
-  `,
-    data: function () {
-      return {
-        styleObject: {
-          border: "1px solid #DADADA",
-          height: "500px",
-          "overflow-y": "scroll"
-        }
-      }
-    }
-  });
-
-  const duckViewModel = new Vue({
+  new Vue({
     el: '#duck-root',
     data: {
       authorName: 'You',
@@ -92,12 +28,15 @@
       },
       addNewMessage: function (message) {
         this.addMessage(this.authorName, message);
+        if (this.timeout) {
+          clearTimeout(this.timeout);
+        }
         this.timeout = setTimeout(function () {
           this.addMessage('Duck', this.pickDuckMessage());
         }.bind(this), 2000);
       },
       pickDuckMessage: function () {
-        return ['Hey', 'Ho', 'Let\'s go'][(Math.floor(Math.random() * 3))];
+        return duckPhrases[(Math.floor(Math.random() * duckPhrases.length))];
       },
       updateAuthorname: function (newName) {
         this.authorName = newName;
